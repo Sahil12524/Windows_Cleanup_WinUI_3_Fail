07aab7a0ebbac8022d4dd5f44e8d205e77dcb444
Sahil Bhandari <sahilshailu7977460287@gmail.com>
2023-05-13 23:54:13 +0530

    Added scripts of PowerOptions and linked them with their respective buttons

--- Views/PowerOptionsView.xaml.cs
+++ Views/PowerOptionsView.xaml.cs
@@ -3,18 +3,9 @@
 
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -31,24 +22,54 @@ namespace Windows_Cleanup_WinUI_3.Views
             this.InitializeComponent();
         }
 
+        private void cmdExec(string? path)
+        {
+            try
+            {
+                // Get the installed location of the application package
+                string appFolderPath = Windows.ApplicationModel.Package.Current.InstalledLocation.Path;
+
+                // Specify the relative path to your .bat file inside the AppX folder
+                string relativeBatPath = path;
+
+                // Combine the app folder path and the relative bat path
+                string batFilePath = Path.Combine(appFolderPath, relativeBatPath);
+
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = batFilePath,
+                    WorkingDirectory = appFolderPath,
+                    UseShellExecute = true
+                };
+
+                Process.Start(processStartInfo);
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occur during execution
+            }
+        }
+
+
+
         private void btnShutdown_Click(object sender, RoutedEventArgs e)
         {
-            //
+            cmdExec(@"Scripts\\PowerOptions\\cmd_shutdown.bat");
         }
 
         private void btnRestart_Click(object sender, RoutedEventArgs e)
         {
-            //
+            cmdExec(@"Scripts\\PowerOptions\\cmd_restart.bat");
         }
 
         private void btnAbortSR_Click(object sender, RoutedEventArgs e)
         {
-            //
+            cmdExec(@"Scripts\\PowerOptions\\cmd_abortpowertask.bat");
         }
 
         private void btnLogOff_Click(object sender, RoutedEventArgs e)
         {
-            //
+            cmdExec(@"Scripts\\PowerOptions\\cmd_logoff.bat");
         }
     }
 }

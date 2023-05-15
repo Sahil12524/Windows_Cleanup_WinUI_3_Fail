5fff1d35387a084e1d7857ee435798d2dc194162
Sahil Bhandari <sahilshailu7977460287@gmail.com>
2023-05-16 02:29:06 +0530

    Minor changes to cleanscript.bat

--- bin/x64/Release/net6.0-windows10.0.19041.0/win10-x64/AppX/Scripts/BasicTools/cleanscript.bat
+++ bin/x64/Release/net6.0-windows10.0.19041.0/win10-x64/AppX/Scripts/BasicTools/cleanscript.bat
@@ -5,10 +5,8 @@ c:
 cls
 del /Q /s C:\Windows\Prefetch\*.*
 pushd "C:\Windows\Prefetch" && (rd /s /q "C:\Windows\Prefetch" 2>nul & popd)
-cls
 del /Q /s C:\Windows\Temp\*.*
 pushd "C:\Windows\Temp" && (rd /s /q "C:\Windows\Temp" 2>nul & popd)
-cls
 del /Q /s C:\Users\%USERNAME%\AppData\Local\Temp\*.*
 pushd "C:\Users\%USERNAME%\AppData\Local\Temp" && (rd /s /q "C:\Users\%USERNAME%\AppData\Local\Temp" 2>nul & popd)
 pause
